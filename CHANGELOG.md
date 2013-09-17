ece2d259220ae8e54708e8c399e076d3e8368ddf
nazoking <nazoking@gmail.com>
2013-09-17 22:15:01 +0900

    起動オプションを追加

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 CHANGELOG
 =========
 
+- 0.0.5: インスタンス立ち上げ時のオプションを追加
+         ( role, tag, security-groups )
+
 - 0.0.4: start からホスト名の設定機能を削除
          --user_data --user_script オプションを追加
          stop コマンドを追加
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(name='awsxx',
       description='AWS High-level utilities',
       url='https://github.com/nazoking/awsxx',
-      version='0.0.4',
+      version='0.0.5',
       author='nazoking',
       author_email='nazoking@gmail.com',
       requires=[
